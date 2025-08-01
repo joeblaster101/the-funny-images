ba30ddd035083b67566a5c7e09506f0732e36b63
WillowedForrests <97166925+joeblaster101@users.noreply.github.com>
2025-08-01 18:09:35 +0100

    Auto commit after pull

--- update.sh
+++ update.sh
@@ -8,14 +8,10 @@ if [ -d "$current_dir/.git" ]; then
   if [ -z "$(git status --porcelain)" ]; then
     echo "No changes to commit in $current_dir"
   else
+    echo "Running git pull in $current_dir"
+    git pull
 
-    echo "Committing changes in $current_dir"
-    git add .
-    git commit -m "Auto commit"
   fi
-
-  echo "Running git pull in $current_dir"
-  git pull
 
   if [ -z "$(git status --porcelain)" ]; then
     echo "No changes to commit after pull in $current_dir"
